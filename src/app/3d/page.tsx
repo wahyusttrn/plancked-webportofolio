5b3a163ee9877221190d15f06d5d870de15f7622
WahyuSattriana <wahyusattriana@gmail.com>
2025-09-29 17:27:01 +0800

    feat: add 3d page

---
+++ src/app/3d/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import BlinkingEdge from '@/components/blinking-edge';
+import NavbarDark from '@/components/navbar-dark';
+import VideoPlayer from '@/components/video-player';
+import { works } from '@/db/works';
+import Image from 'next/image';
+
+const ThreeDPage = () => {
+  return (
+    <>
+      <div className="font-sans text-redish bg-sec-background w-dvw flex flex-col justify-center items-center">
+        <NavbarDark />
+        <div className="w-4/5 mt-28">
+          <div className="">
+            <h1 className="text-redish max-w-3xl">
+              <b>Planck.</b> 3D art station
+            </h1>
+          </div>
+        </div>
+        <section className="relative w-screen flex justify-center mt-10">
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 justify-center min-h-screen w-4/5">
+            {works.map((e, i) => {
+              if (e.type !== 'pamflet') {
+                return (
+                  <VideoPlayer
+                    key={i}
+                    src={`/works/${e.type}/${e.title}.mp4`}
+                    thumbnailSrc={`/works/thumbnail/${e.type}/${e.title}.png`}
+                    className={`w-full ${e.space === 'wide' && 'md:col-span-3 sm:col-span-2'}`}
+                  />
+                );
+              } else {
+                return (
+                  <Image
+                    key={i}
+                    src={`/works/pamflet/${e.title}.png`}
+                    width={400}
+                    height={400}
+                    alt={e.title}
+                    className={`w-full ${e.space === 'wide' && 'md:col-span-3 sm:col-span-2'}`}
+                  />
+                );
+              }
+            })}
+          </div>
+        </section>
+      </div>
+      <BlinkingEdge />
+    </>
+  );
+};
+
+export default ThreeDPage;
